b5265e45e6f36fa5d45937788effb85a4e4f0695
Mattar <acemattar@gmail.com>
2022-01-28 00:52:57 +0200

    feat: usning req and res as streams

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ server.on('request', (req, res) => {
             console.log('Request:', friend);
             friends.push(JSON.parse(friend));
         });
-
+        req.pipe(res);
 
     } else if (req.method === 'GET' && items[1] === 'friends') {
         res.statusCode = 200;
