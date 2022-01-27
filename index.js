ad661af6a95c837c43204d6b0c497565ebea195a
Mattar <acemattar@gmail.com>
2022-01-28 00:34:16 +0200

    feat:POSTing Data to server

--- index.js
+++ index.js
@@ -25,8 +25,15 @@ server.on('request', (req, res) => {
 
     const items = req.url.split('/');
     // /friends/2 => ['','friends','2']
+    if (req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log('Request:', friend);
+            friends.push(JSON.parse(friend));
+        });
 
-    if (items[1] === 'friends') {
+
+    } else if (req.method === 'GET' && items[1] === 'friends') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         if (items.length === 3) {
@@ -35,7 +42,7 @@ server.on('request', (req, res) => {
         } else {
             res.end(JSON.stringify(friends));
         }
-    } else if (items[1] === 'messages') {
+    } else if (req.method === 'GET' && items[1] === 'messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
